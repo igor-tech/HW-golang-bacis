b07a8ac14e82a861c24be314d7348dd52844d95a
Igor Shargin <igor20513@gmail.com>
2025-05-18 00:26:36 +0500

    add calculate currencies func

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,7 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
 	const USDToEUR = 0.90
 	const USDToRUB = 81
 	const EURToRUB = USDToRUB / USDToEUR
+}
+
+func calculateCurrencies(count int, c1, c2 float64) float64 {
+	//
+	return c1 + c2
+}
+
+func getUserInput() {
+	fmt.Print("Enter your currencies: ")
+	//fmt.Scan()
 }
