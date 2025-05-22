3ab515e7a6a3d30a4900f7efb3d9aed1861421de
Igor Shargin <igor20513@gmail.com>
2025-05-22 22:56:52 +0500

    make currencies calculator

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -1,19 +1,111 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+/*
+ТЗ
+
+Финализируем приложение калькулятор. Для этого:
+	- Сделать меню с шагами
+		- Ввод исходной валюты (подсказываем варианты), если ошибка, заново вводим
+		- Ввод числа - если ошибка, заново вводим
+		- Ввод целевой валюты (подсказываем варианты) - если ошибка, заново вводим
+
+Выделить функцию ввода/ проверки валюты и числа
+После получения всех данных с помощью if / switch вычислений итог и вывести результат.
+*/
+
+const USDToEUR = 0.90
+const USDToRUB = 81
+const EURToRUB = USDToRUB / USDToEUR
 
 func main() {
-	const USDToEUR = 0.90
-	const USDToRUB = 81
-	const EURToRUB = USDToRUB / USDToEUR
+	fmt.Println(" __ Калькулятор валют __")
+	currenciesFrom, count, currenciesTo := scanUserData()
+	result := calculateCurrencies(count, currenciesFrom, currenciesTo)
+	fmt.Println("Итого:", result)
 }
 
-func calculateCurrencies(count int, c1, c2 float64) float64 {
-	//
-	return c1 + c2
+func calculateCurrencies(count float64, currenciesFrom, currenciesTo string) float64 {
+	var coefficient float64
+	switch {
+	case currenciesFrom == "USD" && currenciesTo == "EUR":
+		coefficient = USDToEUR
+	case currenciesFrom == "USD" && currenciesTo == "RUB":
+		coefficient = USDToRUB
+	case currenciesFrom == "EUR" && currenciesTo == "RUB":
+		coefficient = EURToRUB
+	case currenciesFrom == "EUR" && currenciesTo == "USD":
+		coefficient = 1 / USDToEUR
+	case currenciesFrom == "RUB" && currenciesTo == "USD":
+		coefficient = USDToEUR / USDToRUB
+	case currenciesFrom == "RUB" && currenciesTo == "EUR":
+		coefficient = 1 / EURToRUB
+	default:
+		coefficient = 1
+	}
+	return coefficient * count
 }
 
-func getUserInput() {
-	fmt.Print("Enter your currencies: ")
-	//fmt.Scan()
+func scanUserData() (string, float64, string) {
+	var currenciesFrom string
+	var currenciesTo string
+	var count float64
+	for {
+		fmt.Print("Введите исходную валюту (USD/EUR/RUB): ")
+		_, err := fmt.Scan(&currenciesFrom)
+
+		if err != nil {
+			fmt.Println("Вы ввели некорректную валюту, попробуйте еще раз")
+			continue
+		}
+
+		if checkCurrencies(currenciesFrom) {
+			break
+		}
+
+		fmt.Println("Введите корректную валюту")
+	}
+
+	for {
+		fmt.Print("Введите число: ")
+		_, err := fmt.Scan(&count)
+		if err != nil {
+			fmt.Println("Вы ввели некорректное число")
+			continue
+		}
+
+		break
+	}
+
+	for {
+		fmt.Print("Введите целевую валюту (USD/EUR/RUB): ")
+		_, err := fmt.Scan(&currenciesTo)
+
+		if err != nil {
+			fmt.Println("Вы ввели некорректную валюту, попробуйте еще раз")
+			continue
+		}
+		if currenciesTo == currenciesFrom {
+			fmt.Println("Валюты не должны повторятся")
+			continue
+		}
+
+		if checkCurrencies(currenciesTo) {
+			break
+		}
+
+		fmt.Println("Введите корректную валюту")
+	}
+	return currenciesFrom, count, currenciesTo
+}
+
+func checkCurrencies(currencies string) bool {
+	if currencies == "USD" || currencies == "EUR" || currencies == "RUB" {
+		return true
+	}
+
+	return false
 }
